8f85825a80581cc5fe084f5ecd349c36085d1235
Wildan Nouval Rizki <wildannouvalrizki@gmail.com>
2023-12-01 16:24:37 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'responsi2mobile',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
